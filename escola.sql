1eb3f86f798f0e3f02698e896e0c242751bb6324
Gabriel "Stromoth" Costa <gabrielpaulocosta2020@gmail.com>
2026-03-04 08:17:29 -0300

    Arquivos da atividade

---
+++ escola.sql
@@ -0,0 +1,23 @@
+CREATE DATABASE escola;
+USE escola;
+
+CREATE TABLE Disciplina (
+    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(20) NOT NULL,
+    descricao VARCHAR(100)
+);
+
+CREATE TABLE Professor (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(40) NOT NULL,
+    disciplina_id INT UNSIGNED,
+
+    FOREIGN KEY (disciplina_id)
+    REFERENCES Disciplina(id)
+);
+
+CREATE TABLE Aluno (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(40) NOT NULL,
+    idade INT
+);
